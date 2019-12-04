f71e0fcfd143bb250d8a06bf6252745bf096ddbf
su-san1129 <deviationvalue1129@icloud.com>
2019-12-04 23:02:42 +0900

    共通部品の切り出し完了

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -1,15 +1,32 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * ユーザー情報を操作するコントローラー.
+ * 
+ * @author takahiro.suzuki
+ *
+ */
 @Controller
 @RequestMapping("")
 public class UserController {
 	
 	@RequestMapping("/login")
-	public String login() {
-		return "";
+	public String login(Model model, @RequestParam(required=false) String error) {
+		if( error != null) {
+			model.addAttribute("error", "メールアドレスまたはパスワードが違います。");
+			return login(model, error);
+		}
+		return "login";
+	}
+	
+	@RequestMapping("/register")
+	public String toRegister() {
+		return "register_user";
 	}
 
 }
