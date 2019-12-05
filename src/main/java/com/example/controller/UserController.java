80627ab278160a93999438478af5d26ea848f659
su-san1129 <deviationvalue1129@icloud.com>
2019-12-05 16:52:11 +0900

    入力値チェック作成

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -3,6 +3,7 @@ package com.example.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -21,6 +22,11 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+
+	@ModelAttribute
+	private RegisterUserForm setUpRegisterUserForm() {
+		return new RegisterUserForm();
+	}
 
 	/**
 	 * ログイン画面を表示する.
